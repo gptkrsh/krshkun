4ec2573c3239a95cedc04f7859c4a8b5b624b629
Krish Gupta <krshkodes@gmail.com>
2022-08-12 12:53:18 +0000

    style: remove line breaks

--- src/config.ts
+++ src/config.ts
@@ -19,6 +19,6 @@ export const config: ConfigurationSyntax = {
     bannerUrl: 'assets/Banner.png'
   },
   other: {
-    sectionSeparator: '\n<br width="100%" />\n'
+    sectionSeparator: '\n'
   }
 }
